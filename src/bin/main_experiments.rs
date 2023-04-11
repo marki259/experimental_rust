ea67fa8843b36a56a8266373680b350bcc138fd5
marc <marcandrechenier@gmail.com>
2023-04-11 20:25:56 +0200

    Gotta debug the filtering in the exact_pval impl

--- src/bin/main_experiments.rs
+++ src/bin/main_experiments.rs
@@ -4,11 +4,11 @@ fn main() {
     // let n = Some(20);
     // let k = Some(10);
 
-    let x_treatment = vec![3, 2, 1];
+    let x_treatment = vec![1, 1, 1];
     let x_treatment = x_treatment.iter().map(|&x| x as f64).collect();
     let x_treatment = Some(x_treatment);
     
-    let x_control = vec![3, 0, 0, 2];
+    let x_control = vec![1, 0, 1, 2];
     let x_control = x_control.iter().map(|&x| x as f64).collect();
     let x_control = Some(x_control);
 
@@ -18,5 +18,7 @@ fn main() {
     println!("{:?}", experiment);
 
     println!("{:#?}", experiment.mean());
+
+    println!("{:#?}", experiment.exact_pval());
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-use rand::{Rng, seq::SliceRandom};
+use std::thread::current;
+
+use rand::{seq::SliceRandom, Rng};
 
 #[derive(Debug)]
 pub struct Experiment {
-    n: i64, 
+    n: i64,
     k: i64,
-    n_comb: u128,  
+    n_comb: u128,
     x_treatment: Option<Vec<f64>>,
     x_control: Option<Vec<f64>>,
 }
@@ -16,7 +18,12 @@ pub struct BivMean {
 }
 
 impl Experiment {
-    pub fn new(n: Option<i64>, k: Option<i64>, x_treatment: Option<Vec<f64>>, x_control: Option<Vec<f64>>) -> Experiment {
+    pub fn new(
+        n: Option<i64>,
+        k: Option<i64>,
+        x_treatment: Option<Vec<f64>>,
+        x_control: Option<Vec<f64>>,
+    ) -> Experiment {
         let n = match n {
             Some(x) => x,
             _ => {
@@ -38,16 +45,18 @@ impl Experiment {
 
         let n_combs = count_combinations(n, k);
 
-        Experiment { n: n, 
-            k: k, 
-            n_comb: n_combs, 
-            x_treatment: x_treatment, 
-            x_control: x_control }
+        Experiment {
+            n: n,
+            k: k,
+            n_comb: n_combs,
+            x_treatment: x_treatment,
+            x_control: x_control,
+        }
     }
 
     pub fn mean(&self) -> BivMean {
         let x_treatment = self.x_treatment.as_ref().unwrap();
-        let x_control = self.x_control.as_ref().unwrap(); 
+        let x_control = self.x_control.as_ref().unwrap();
 
         let n_treatment = x_treatment.len() as f64;
         let n_control = x_control.len() as f64;
@@ -55,13 +64,70 @@ impl Experiment {
         let mut m_treatment: f64 = x_treatment.iter().sum::<f64>();
         let mut m_control: f64 = x_control.iter().sum::<f64>();
 
-        m_treatment /= n_treatment;        
+        m_treatment /= n_treatment;
         m_control /= n_control;
 
         BivMean {
-            m_treatment, 
-            m_control
+            m_treatment,
+            m_control,
         }
+    }
+
+    pub fn exact_pval(&self) -> f64 {
+        let n = self.n;
+        let k = self.k;
+
+        let x_treatment = self.x_treatment.as_ref().unwrap();
+        let x_control = self.x_control.as_ref().unwrap();
+
+        let mut x_all = x_treatment.clone();
+        x_all.extend(x_control.iter());
+
+        let means = self.mean();
+        let observed_effect = means.m_treatment - means.m_control;
+
+        let mut all_orders = vec![];
+        let current_order = vec![];
+        generate_all_orders(n, k, current_order, &mut all_orders);
+
+        let mut counter: i64 = 0;
+
+        for placebo in all_orders.iter() {
+            let mut filter_bool = placebo.into_iter();
+            let mut inverted_filter_bool =
+                placebo.into_iter().map(|p| if *p { false } else { true });
+
+            let x_placebo_treatment: Vec<f64> = x_all
+                .clone()
+                .into_iter()
+                .filter(|_| filter_bool.next().is_some())
+                .collect();
+            let x_placebo_control: Vec<f64> = x_all
+                .clone()
+                .into_iter()
+                .filter(|_| inverted_filter_bool.next().is_some())
+                .collect();
+
+            let placebo_experiment = Experiment::new(
+                None,
+                None,
+                Some(x_placebo_treatment),
+                Some(x_placebo_control),
+            );
+
+            let placebo_means = placebo_experiment.mean();
+
+            let placebo_delta = placebo_means.m_treatment - placebo_means.m_control;
+
+            if observed_effect.abs() > placebo_delta.abs() {
+                counter += 1;
+            } else {
+                continue;
+            }
+        }
+
+        1.0 - ((counter as f64).ln() - (self.n_comb as f64).ln()).exp()
+
     }
 }
 
@@ -89,9 +155,9 @@ pub fn count_combinations(n: i64, k: i64) -> u128 {
     let n = n.clone() as u64;
     let k = k.clone() as u64;
 
-    let log_comb = log_factorial(n) - log_factorial(k) - log_factorial(n-k);
+    let log_comb = log_factorial(n) - log_factorial(k) - log_factorial(n - k);
 
-    let val = log_comb.exp().round() as u128;    
+    let val = log_comb.exp().round() as u128;
 
     val
 }
@@ -100,7 +166,7 @@ pub fn factorial(x: u64) -> u128 {
     let s = log_factorial(x);
 
     let s = s.exp().round() as u128;
-    
+
     s
 }
 
@@ -116,9 +182,14 @@ pub fn log_factorial(x: u64) -> f64 {
     s
 }
 
-pub fn generate_all_orders(n: i64, k: i64, current_order: Vec<bool>, mut all_orders: &mut Vec<Vec<bool>>) {
+pub fn generate_all_orders(
+    n: i64,
+    k: i64,
+    current_order: Vec<bool>,
+    mut all_orders: &mut Vec<Vec<bool>>,
+) {
     let current_len: i64 = current_order.len().try_into().unwrap();
-    let current_sum: i64 = current_order.iter().map(|b| if *b {1} else {0}).sum();
+    let current_sum: i64 = current_order.iter().map(|b| if *b { 1 } else { 0 }).sum();
 
     let mut current_order1 = current_order.clone();
     let mut current_order2 = current_order.clone();
@@ -138,5 +209,4 @@ pub fn generate_all_orders(n: i64, k: i64, current_order: Vec<bool>, mut all_ord
 
     generate_all_orders(n, k, current_order1, &mut all_orders);
     generate_all_orders(n, k, current_order2, &mut all_orders);
-    
 }
